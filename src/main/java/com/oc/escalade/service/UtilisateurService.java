41fe73f498a32231e2acb455b4f91a08ea69275b
Christophe <christophe.orsini@gmail.com>
2020-04-03 18:00:45 +0200

    Correction service utilisateur

--- src/main/java/com/oc/escalade/service/UtilisateurService.java
+++ src/main/java/com/oc/escalade/service/UtilisateurService.java
@@ -1,5 +1,6 @@
 package com.oc.escalade.service;
 
+import com.oc.escalade.entities.RoleEnum;
 import com.oc.escalade.entities.Utilisateur;
 
 public interface UtilisateurService
@@ -8,22 +9,17 @@ public interface UtilisateurService
 	 * Inscription d'un nouvel utilisateur
 	 * @param email String : le login qui doit être unique dans la base
 	 * @param password String : le mot de passe qui sera chiffré
-	 * @return Utilisateur : l'utilisateur créé
-	 */
-	public Utilisateur inscription(String email, String password); // F3
-	/**
-	 * Inscription d'un nouvel utilisateur
-	 * @param email String : le login qui doit être unique dans la base
-	 * @param password String : le mot de passe qui sera chiffré
 	 * @param nom String : le nom de l'utilisateur
 	 * @param prenom String : le prenom de l'utilisateur
+	 * @param role RoleEnum : le role de l'utilisateur {@link RoleEnum}
 	 * @return Utilisateur : l'utilisateur créé
 	 */
-	public Utilisateur inscription(String email, String password, String nom, String prenom); // F3
+	public Utilisateur inscription(String email, String password, String nom, String prenom, RoleEnum role); // F3
+	
 	/**
-	 * Vérifie si un utilisateur existe dèjà en rechecherchant son email dans la base
-	 * @param utilisateur Utilisateur : l'utilisateur à rechercher
-	 * @return boolean : true si l'utilisateur existe false sinon
+	 * Retourne un utilisateur existant
+	 * @param email String : l'email de l'utilisateur
+	 * @return Utilisateur :
 	 */
-	public boolean exists(Utilisateur utilisateur);
+	public Utilisateur consulter(String email);
 }
--- src/main/java/com/oc/escalade/service/UtilisateurServiceImpl.java
+++ src/main/java/com/oc/escalade/service/UtilisateurServiceImpl.java
@@ -1,74 +1,48 @@
 package com.oc.escalade.service;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.oc.escalade.dao.UtilisateurDAO;
+import com.oc.escalade.dao.UtilisateurRepository;
+import com.oc.escalade.entities.RoleEnum;
 import com.oc.escalade.entities.Utilisateur;
 
 @Service
 public class UtilisateurServiceImpl implements UtilisateurService
 {
 	@Autowired
-	private UtilisateurDAO utilisateurDAO;
+	private UtilisateurRepository utilisateurDAO;
+
+	@Override
+	public Utilisateur inscription(String email, String password, String nom, String prenom, RoleEnum role)
+	{
+		// verifier si email existe
+		if (utilisateurDAO.findByEmail(email).isPresent())
+		{
+			throw new RuntimeException("L'utilisateur " + email + " existe déjà");
+		}
+		
+		// TODO chiffrer le mot de passe
+		
+		// créer l'utilisateur
+		Utilisateur utilisateur = new Utilisateur(email, password, nom, prenom, role);
+		
+		// sauvegarder et retourner l'utilisateur
+		return utilisateurDAO.save(utilisateur);
+	}
+
+	@Override
+	public Utilisateur consulter(String email)
+	{
+		Optional<Utilisateur> utilisateurOpt = utilisateurDAO.findByEmail(email);
+		// verifier si email existe
+		if (!utilisateurOpt.isPresent())
+		{
+			throw new RuntimeException("L'utilisateur " + email + " n'existe pas");
+		}
+		
+		return utilisateurOpt.get();		
+	}
 	
-	@Override
-	public Utilisateur inscription(String email, String password) {
-		// verifier si email existe
-		if (utilisateurDAO.findByEmail(email).isPresent())
-		{
-			throw new RuntimeException("L'utilisateur " + email + " existe déjà");
-		}
-		
-		// verifier que l'email est conforme
-		String pattern = "^[A-Za-z]+[A-Za-z0-9_\\-\\.]*@[A-Za-z0-9_\\-]*.[A-Za-z]{2,4}";
-		if (!email.matches(pattern))
-		{
-			throw new RuntimeException("Le format de l'email n'est pas conforme");
-		}
-		
-		// chiffrer le mot de passe
-		
-		// créer l'utilisateur
-		Utilisateur utilisateur = new Utilisateur(email, password);
-		utilisateur.setNom("");
-		utilisateur.setPrenom("");
-		
-		// sauvegarder et retourner l'utilisateur
-		return utilisateurDAO.save(utilisateur);
-	}
-
-	@Override
-	public Utilisateur inscription(String email, String password, String nom, String prenom)
-	{
-		// verifier si email existe
-		if (utilisateurDAO.findByEmail(email).isPresent())
-		{
-			throw new RuntimeException("L'utilisateur " + email + " existe déjà");
-		}
-		
-		// chiffrer le mot de passe
-		
-		// créer l'utilisateur
-		Utilisateur utilisateur = new Utilisateur(email, password);
-		utilisateur.setNom(nom);
-		utilisateur.setPrenom(prenom);
-		
-		// sauvegarder et retourner l'utilisateur
-		return utilisateurDAO.save(utilisateur);
-	}
-
-	@Override
-	public boolean exists(Utilisateur utilisateur)
-	{
-		if (utilisateurDAO.findByEmail(utilisateur.getEmail()).isPresent())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
 }
