9a3dac8754ca0b74aa096666cd0f027d00fc59b7
Christophe <christophe.orsini@gmail.com>
2020-04-10 16:21:32 +0200

    Correction Repository

--- src/main/java/com/oc/escalade/dao/SiteRepository.java
+++ src/main/java/com/oc/escalade/dao/SiteRepository.java
@@ -1,8 +1,6 @@
 package com.oc.escalade.dao;
 
 import java.util.Collection;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,21 +11,15 @@ import com.oc.escalade.entities.Site;
 @Repository
 public interface SiteRepository extends JpaRepository<Site, Long>
 {
-	public Optional<Site> findByNomIgnoreCase(String nom);
-	/*	
 	@Query("SELECT t FROM #{#entityName} t WHERE (:nom is null or nom LIKE %:nom%)"
 			+ " AND (:commune is null or commune LIKE %:commune%)"
 			+ " AND (:departement is null or departement LIKE %:departement%)"
 			+ " AND (:pays is null or pays LIKE %:pays%)"
+			+ " AND (:secteurs is null or nbreSecteurs = :secteurs)"
+			+ " AND (:voies is null or nbreVoies = :voies)"
+			+ " AND (:cotation is null or :cotation >= cotationMini)"
+			+ " AND (:cotation is null or :cotation <= cotationMaxi)"
 			+ " ORDER BY pays, departement, commune, nom")
 	public Collection<Site> findAllByElements(@Param("nom") String nom, @Param("commune") String commune, @Param("departement") String departement,
-			@Param("pays") String pays, @Param("cotation") String cotation, @Param("secteurs") int nombreSecteurs, @Param("voies") int nombreVoies);
-			*/
-	@Query("SELECT t FROM #{#entityName} t WHERE (:nom is null or nom LIKE %:nom%)"
-			+ " AND (:commune is null or commune LIKE %:commune%)"
-			+ " AND (:departement is null or departement LIKE %:departement%)"
-			+ " AND (:pays is null or pays LIKE %:pays%)"
-			+ " ORDER BY pays, departement, commune, nom")
-	public Collection<Site> findAllByElements(@Param("nom") String nom, @Param("commune") String commune, @Param("departement") String departement,
-			@Param("pays") String pays);
+			@Param("pays") String pays, @Param("cotation") String cotation, @Param("secteurs") String nombreSecteurs, @Param("voies") String nombreVoies);
 }
