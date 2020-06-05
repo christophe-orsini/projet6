03704c0bd969741a04cc684bc6e48985e2d4567d
Christophe <christophe.orsini@gmail.com>
2020-06-05 12:05:22 +0200

    Mise à jour du run

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 @echo off
 chcp 65001
 cls
-if not exist target\escalade-0.0.1-SNAPSHOT.jar (
+if not exist target\escalade-1.0.2.jar (
 	echo **************************************************************************************
 	echo * L'application n'est pas installée !                                                *
 	echo * Exécuter la commande 'install' en mode console ou double-cliquer sur 'install.bat' *
 	echo * pour installer l'application                                                       *
 	echo **************************************************************************************
 ) else (
-	java -jar target\escalade-0.0.1-SNAPSHOT.jar
+	java -jar target\escalade-1.0.2.jar
 )
